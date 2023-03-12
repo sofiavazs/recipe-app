74e315e1b5a585442ad8d38d4523bcdb9ca025d1
Sofia Sousa <sofiavazsousa@gmail.com>
2023-03-12 23:40:32 +0100

    Refactor form steps

--- src/components/NewRecipeForm.tsx
+++ src/components/NewRecipeForm.tsx
@@ -41,98 +41,110 @@ const NewRecipeForm: React.FC<Props> = () => {
     nextFormStep();
   };
 
+  const RecipeNameAndIngredients = () => {
+    return (
+      <>
+        <Box>
+          <span>Step {formStep + 1} of 3</span>
+          <label htmlFor="text">
+            Nome da Receita
+            <input
+              {...register("recipeName")}
+              placeholder="Nome da Receita"
+              type="text"
+            ></input>
+          </label>
+        </Box>
+        {fields.map((ingredient, index) => (
+          <Box width={"100%"} margin={"1rem"} key={ingredient.id}>
+            <label>
+              Ingrediente
+              <input
+                placeholder="Ingrediente"
+                {...register(`ingredients.${index}.name` as const, {
+                  required: true,
+                })}
+                defaultValue={ingredient.name}
+                type="text"
+              />
+            </label>
+            <label>
+              Quantidade
+              <input
+                placeholder="Quantidade"
+                {...register(`ingredients.${index}.quantity.number` as const, {
+                  required: true,
+                })}
+                defaultValue={ingredient.quantity.number}
+                type="number"
+              />
+            </label>
+            <label>
+              Unidade de medida
+              <input
+                placeholder="Unidade"
+                {...register(
+                  `ingredients.${index}.quantity.unitMeasure` as const,
+                  {
+                    required: true,
+                  }
+                )}
+                defaultValue={ingredient.quantity.unitMeasure}
+                type="text"
+              />
+            </label>
+            <br />
+            {fields.length > 1 && (
+              <button type="button" onClick={() => remove(index)}>
+                Remover
+              </button>
+            )}
+          </Box>
+        ))}
+        <button
+          type="button"
+          onClick={() =>
+            append({
+              name: "",
+              quantity: { number: 0, unitMeasure: "grams" },
+            })
+          }
+        >
+          Adicionar Ingrediente
+        </button>
+        <button>Next</button>
+      </>
+    );
+  };
+
+  const RecipeInstructions = () => {
+    return (
+      <>
+        <span>Step {formStep + 1} of 3</span>
+        <textarea placeholder="test"></textarea>
+        <button onClick={prevFormStep}>Previous</button>
+        <button>Next</button>
+      </>
+    );
+  };
+
+  const RecipeSummary = () => {
+    return (
+      <>
+        <span>Step {formStep + 1} of 3</span>
+        <div>{JSON.stringify(data)}</div>
+        <button onClick={prevFormStep}>Previous</button>
+        <button type="submit">Submit</button>
+      </>
+    );
+  };
+
   return (
     <>
       <form onSubmit={handleSubmit(onSubmit)}>
-        {formStep === 0 && (
-          <>
-            <Box>
-              <span>Step {formStep + 1} of 3</span>
-              <label htmlFor="text">
-                Nome da Receita
-                <input
-                  {...register("recipeName")}
-                  placeholder="Nome da Receita"
-                  type="text"
-                ></input>
-              </label>
-            </Box>
-            {fields.map((ingredient, index) => (
-              <Box width={"100%"} margin={"1rem"} key={ingredient.id}>
-                <label>
-                  Ingrediente
-                  <input
-                    placeholder="Ingrediente"
-                    {...register(`ingredients.${index}.name` as const, {
-                      required: true,
-                    })}
-                    defaultValue={ingredient.name}
-                    type="text"
-                  />
-                </label>
-                <label>
-                  Quantidade
-                  <input
-                    placeholder="Quantidade"
-                    {...register(
-                      `ingredients.${index}.quantity.number` as const,
-                      {
-                        required: true,
-                      }
-                    )}
-                    defaultValue={ingredient.quantity.number}
-                    type="number"
-                  />
-                </label>
-                <label>
-                  Unidade de medida
-                  <input
-                    placeholder="Unidade"
-                    {...register(
-                      `ingredients.${index}.quantity.unitMeasure` as const,
-                      {
-                        required: true,
-                      }
-                    )}
-                    defaultValue={ingredient.quantity.unitMeasure}
-                    type="text"
-                  />
-                </label>
-                <br />
-                {fields.length > 1 && (
-                  <button type="button" onClick={() => remove(index)}>
-                    Remover
-                  </button>
-                )}
-              </Box>
-            ))}
-            <button
-              type="button"
-              onClick={() =>
-                append({
-                  name: "",
-                  quantity: { number: 0, unitMeasure: "grams" },
-                })
-              }
-            >
-              Adicionar Ingrediente
-            </button>
-            <button>Next</button>
-          </>
-        )}
-        {formStep === 1 && (
-          <>
-            <textarea placeholder="test"></textarea>
-            <button onClick={prevFormStep}>Previous</button>
-            <button>Next</button>
-          </>
-        )}
-        {formStep === 2 && (
-          <>
-            <div>{JSON.stringify(data)}</div>
-            <button type="submit">Submit</button>
-          </>
-        )}
+        {formStep === 0 && <RecipeNameAndIngredients />}
+        {formStep === 1 && <RecipeInstructions />}
+        {formStep === 2 && <RecipeSummary />}
       </form>
     </>
   );
